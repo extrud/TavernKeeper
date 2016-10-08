964499024666651e0aa320ebf3b335eec669408b
Fishkov Artem <Extrud99@gmail.com>
2016-10-08 19:54:17 +0300

    Класс Тэг,Персона,класс персонажа, добавлены тестовые спрайты

---
+++ Assets/CharClass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+class CharClass
+    {
+        string Name;
+        int Vine;
+        int Beer;
+        int Whiskey;
+
+        Sprite[] ClassSprites;
+        
+    }
+
---
+++ Assets/Person.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+class Person
+{
+    CharClass chClass;
+    string name;
+    int solvency;
+    int drunk;
+    List<String> tags;
+    bool IsHaveTag(string tag)
+    {
+        return tags.Contains(tag);
+    }
+    void RemoveTag(string tag)
+    {
+        tags.Remove(tag);
+    }
+    void AddTag(string tag)
+    {
+        tags.Add(tag);
+    }
+    internal CharClass ChClass
+    {
+        get
+        {
+            return chClass;
+        }
+
+        set
+        {
+            chClass = value;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            name = value;
+        }
+    }
+
+    public int Solvency
+    {
+        get
+        {
+            return solvency;
+        }
+
+        set
+        {
+            solvency = value;
+        }
+    }
+
+    public int Drunk
+    {
+        get
+        {
+            return drunk;
+        }
+
+        set
+        {
+            drunk = value;
+        }
+    }
+
+    public List<string> Tags
+    {
+        get
+        {
+            return tags;
+        }
+
+        set
+        {
+            tags = value;
+        }
+    }
+}
+
